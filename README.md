2a13cf9780be12c8c8fe542ad5615f5648b9fa64
rcutte <rcutte@student.42.fr>
2025-08-30 16:07:07 +0200

    refactor: change typo

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # 42_InceptionOfThings-rcutte_manifest
 
 App repo of the manifests needed for InceptionOfThings CiCd project with ArgoCD
+
+- Image: https://hub.docker.com/r/wil42/playground
+- Port: `8888`
+
+```bash
+# Usage: ./update_version.sh <version> (allowed: 1 or 2)
+./update_version.sh 1
+```
+
+```bash
+# Check after sync
+curl localhost:8888
+# Output
+```
--- update_version.sh
+++ update_version.sh
@@ -3,21 +3,28 @@
 set -euo pipefail
 
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <new_version>"
+  echo "Usage: $0 <version> (allowed: 1 or 2)"
   exit 1
 fi
 
 NEW_VERSION="$1"
+
+# Allow only 1 or 2
+if [[ "$NEW_VERSION" != "1" && "$NEW_VERSION" != "2" ]]; then
+  echo "❌ Invalid version: $NEW_VERSION. Only '1' or '2' are allowed."
+  exit 1
+fi
+
 DEPLOY_FILE="./manifests/deployment.yaml"
 
 # 1️⃣ Swap version in deployment.yaml
-echo "🔄 Updating $DEPLOY_FILE to version $NEW_VERSION..."
-sed -i "s|\(wil42/playground:\)[^[:space:]]*|\1$NEW_VERSION|g" "$DEPLOY_FILE"
+echo "🔄 Updating $DEPLOY_FILE to version v$NEW_VERSION..."
+sed -i "s|\(wil42/playground:\)[^[:space:]]*|\1v$NEW_VERSION|g" "$DEPLOY_FILE"
 
 # 2️⃣ Git add, commit, push
 git add "$DEPLOY_FILE"
-git commit -m "chore: update playground image to $NEW_VERSION"
+git commit -m "chore: update playground image to v$NEW_VERSION"
 git push
 
-echo "✅ Updated version to $NEW_VERSION and pushed to Git."
+echo "✅ Updated version to v$NEW_VERSION and pushed to Git."
 
